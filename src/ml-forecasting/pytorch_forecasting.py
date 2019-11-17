8ea82286cde281de85c5bb63f09fe6ba4baf06ae
Sonam Ghosh <sonamghosh@gmail.com>
2019-11-17 08:42:07 -0500

    Added plotting for several day forecasting on graph

--- src/ml-forecasting/pytorch_forecasting.py
+++ src/ml-forecasting/pytorch_forecasting.py
@@ -17,40 +17,12 @@ class LinearRegressionModel(nn.Module):
         out = self.linear(x)
         return out
 
-# LSTM
-class Sequence(nn.Module):
-    def __init__(self):
-        super(Sequence, self).__init__()
-        self.lstm1 = nn.LSTMCell(1, 51)
-        self.lstm2 = nn.LSTMCell(51, 51)
-        self.linear = nn.Linear(51, 1)
-
-    def forward(self, input, future=0):
-        outputs = []
-        h_t = torch.zeros(input.size(0), 51, dtype=torch.double)
-        c_t = torch.zeros(input.size(0), 51, dtype=torch.double)
-        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
-        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
-
-        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs += [output]
-        for i in range(future):
-            h_t, c_t = self.lstm1(output, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs += [output]
-        outputs = torch.stack(outputs, 1)
-        return outputs
-
 
 if __name__ == "__main__":
     np.random.seed(0)
     torch.manual_seed(0)
 
-    test = pd.read_csv('./dataset/Northeastern-University_tuition.csv')
+    test = pd.read_csv('./dataset/Boston-University_tuition.csv')
 
     df = test[['TUITIONFEE_OUT']]
 
@@ -107,59 +79,93 @@ if __name__ == "__main__":
     forecast = np.polyfit(list(range(len(predicted_arr))), predicted_arr, 1)
 
     print(forecast)
-    p = np.poly1d(forecast)
+    p = np.poly1d(forecast)  # forecasted equation
 
     xp = np.arange(20)
-    plt.plot(xp, p(xp), '*')
-    #plt.clf()
-    #plt.plot(x_train, y_train, 'go', label='True data', alpha=0.5)
-    #plt.plot(x_train, predicted, '--', label='Predictions', alpha=0.5)
-    #plt.legend(loc='best')
+
+
+    timestamps = test[['YearDT']]
+    timestamps = timestamps.YearDT.tolist()
+    timestamps = [time[0:4] for time in timestamps]
+    num_pts = len(xp) - len(x_train)
+
+    max_time = timestamps[-1]
+    for pt in range(1, num_pts+1):
+        val = int(max_time) + pt
+        timestamps.append(str(val))
+    print(timestamps)
+
+    #plt.plot(xp, p(xp), '*')
+    plt.clf()
+    plt.plot(x_train, y_train, 'go', label='True data', alpha=0.5)
+    plt.plot(x_train, predicted, '--', label='Predictions', alpha=0.5)
+    plt.plot(timestamps, p(xp), 'ro', label='Forecasted', alpha=0.5)
+    plt.plot(timestamps[-num_pts-1:], p(xp[-num_pts-1:]), 'r--', alpha=0.5)
+    plt.title('Forecasted Cost for Boston University for {num_pts} years from the year {max_time}'.format(num_pts=str(num_pts), max_time=max_time))
+    plt.legend(loc='best')
     plt.show()
 
-    """
-    data = torch.load('traindata.pt')
-    input = torch.from_numpy(data[3:, :-1])
-    target = torch.from_numpy(data[3:, 1:])
-    test_input = torch.from_numpy(data[:3, :-1])
-    test_target = torch.from_numpy(data[:3, 1:])
-    # build model
-    seq = Sequence()
-    seq.double()
-    criterion = nn.MSELoss()
-    optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
 
-    # train
-    for i in range(15):
-        print('STEP: ', i)
-        def closure():
-            optimizer.zero_grad()
-            out = seq(input)
-            loss = criterion(out, target)
-            print('loss:', loss.item())
-            loss.backward()
-            return loss
-        optimizer.step(closure)
-        # begin to predict, no need to track gradient here
-        with torch.no_grad():
-            future = 1000
-            pred = seq(test_input, future=future)
-            loss = criterion(pred[:, :-future], test_target)
-            print('test loss:', loss.item())
-            y = pred.detach().numpy()
-        # draw the result
-        plt.figure(figsize=(30,10))
-        plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
-        plt.xlabel('x', fontsize=20)
-        plt.ylabel('y', fontsize=20)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        def draw(yi, color):
-            plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
-            plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
-        draw(y[0], 'r')
-        draw(y[1], 'g')
-        draw(y[2], 'b')
-        plt.savefig('predict%d.pdf'%i)
-        plt.close()
+
+    """
+    xt = range(predicted.shape[0])
+    fit = p(xt)
+
+    # Coordinates of fit curve
+    c_y = [np.min(fit), np.max(fit)]
+    c_x = [np.min(xt), np.max(xt)]
+
+    # predict y value of original date using fit
+    p_y = forecast[0] * xt + forecast[1]
+
+    # Calculate y-error (resids)
+    y_err = y_train - p_y
+
+    # create series of new test x-values to predict for
+    p_x = np.arange(np.min(xt),np.max(xt)+1,1)
+
+    # now calculate confidence intervals for new test x-series
+    mean_x = np.mean(xt)         # mean of x
+    n = len(xt)              # number of samples in origional fit
+    t = 2.31                # appropriate t value (where n=9, two tailed 95%)
+    s_err = np.sum(np.power(y_err,2))   # sum of the squares of the residuals
+
+    confs = t * np.sqrt((s_err/(n-2))*(1.0/n + (np.power((p_x-mean_x),2)/
+                ((np.sum(np.power(xt,2)))-n*(np.power(mean_x,2))))))
+
+    # now predict y based on test x-values
+    p_y = forecast[0]*p_x+forecast[0]
+
+    # get lower and upper confidence limits based on predicted y and confidence intervals
+    lower = p_y - abs(confs)
+    upper = p_y + abs(confs)
+
+    # set-up the plot
+    plt.axes().set_aspect('equal')
+    plt.xlabel('X values')
+    plt.ylabel('Y values')
+    plt.title('Linear regression and confidence limits')
+
+    # plot sample data
+    plt.plot(x_train,predicted,'bo',label='Sample observations', alpha=0.5)
+
+    # plot line of best fit
+    plt.plot(c_x,c_y,'r-',label='Regression line')
+
+    # plot confidence limits
+    plt.plot(p_x,lower,'b--',label='Lower confidence limit (95%)')
+    plt.plot(p_x,upper,'b--',label='Upper confidence limit (95%)')
+
+    # set coordinate limits
+    plt.xlim(0,11)
+    plt.ylim(0,11)
+
+    # configure legend
+    plt.legend(loc=0)
+    leg = plt.gca().get_legend()
+    ltext = leg.get_texts()
+    plt.setp(ltext, fontsize=10)
+
+    # show the plot
+    plt.show()
     """
